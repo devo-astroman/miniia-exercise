40b46a37cd29e340e0a9d43e2adb581fec07e399
Devo Astroman <devo.astroman@gmail.com>
2025-11-20 09:28:40 +0100

    add vehicle movement

--- Assets/Scripts/Vehicle/VehicleMovement.cs
+++ Assets/Scripts/Vehicle/VehicleMovement.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class VehicleMovement : MonoBehaviour
 {
+
+    [SerializeField] private Rigidbody _rbody;
+    [SerializeField] private float _speed = 10f;
+
+    private bool _goForward = true;
+    private bool _goBackward = false;
+    private bool _goLeft = false;
+    private bool _goRight = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,12 +22,63 @@ public class VehicleMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (_goForward)
+        {
+            _rbody.AddForce(Vector3.forward * _speed, ForceMode.Acceleration);
+        }
+        else if (_goBackward)
+        {
+            _rbody.AddForce(Vector3.forward * _speed *-1, ForceMode.Acceleration);
+        }
+        else if (_goLeft)
+        {
+            _rbody.AddForce(Vector3.right * _speed *-1, ForceMode.Acceleration);
+        }
+        else if (_goRight)
+        {
+            _rbody.AddForce(Vector3.right * _speed, ForceMode.Acceleration);
+        }       
+
     }
 
 
-    public void MoveForward(float distance)
+    public void MoveForward()
     {
-        transform.Translate(Vector3.forward * distance);
+        _goForward = true;
+        _goBackward = false;
+        _goLeft = false;
+        _goRight = false;
+    }
+
+    public void MoveBackward()
+    {
+        _goForward = false;
+        _goBackward = true;
+        _goLeft = false;
+        _goRight = false;
+    }
+
+    public void MoveLeft()
+    {
+        _goForward = false;
+        _goBackward = false;
+        _goLeft = true;
+        _goRight = false;
+    }
+
+    public void MoveRight()
+    {
+        _goForward = false;
+        _goBackward = false;
+        _goLeft = false;
+        _goRight = true;
+    }
+
+    public void Stop()
+    {
+        _goForward = false;
+        _goBackward = false;
+        _goLeft = false;
+        _goRight = false;
     }
 }
